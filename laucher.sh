f34daa15322cca57e8a081a9aafe2ae4eb2ff207
Tim <timothee.escandell@gmail.com>
2024-02-28 15:52:58 +0100

    php websites docker

--- laucher.sh
+++ laucher.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-if [ $# -ne 1 ];
-then
-	echo "Usage $0 <config_dir>"
-	exit 1
-fi
-
-if [ ! -d $1 ];
-then
-	echo "Need config as directory"
-	exit 1
-fi
-
-./builder.sh $1
+echo "Don't forget to build first !!!"
+echo ""
+sleep 1
 
 docker compose -f ./srcs/docker-compose.yml up
